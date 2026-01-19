d0eff4a608fffa73c584408b6edc4a63138dd5dd
困困鱼 <1152958806@qq.com>
2026-01-19 19:24:05 +0800

    0

--- ui/env.d.ts
+++ ui/env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="unplugin-icons/types/vue" />
 
+declare module "*.vue" {
+  import Vue from "vue";
+  export default Vue;
+}
+
 declare module "vue" {
   interface ComponentCustomProperties {
     $formkit: any;
